8184eac585e70f178b95f4f039f294859bbb573e
Triston <2b11.1935488.tristonloh@gmail.com>
2021-01-30 19:47:22 +0800

    Completed sorting and basic features

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import necessary classes
 import os.path
 from BinaryTree import BinaryTree
 from Expression import Expression
@@ -5,11 +6,10 @@ from Expression2 import Expression2
 from node import Node
 from sortedList import sortedList
 from stack import Stack
-from HashTable import HashTable
 from Equations import Equations
 from treeLayout import treeLayout
 
-# function to print the selection menu
+# print the selection menu
 def selectionMenu():
     print('*' * 62)
     print('* ST1507 DSAA: Expression Evaluator & Sorter {:>17}'.format('*'))
@@ -38,7 +38,7 @@ def selectionMenu():
         else:
             print("Invalid input! Please input 1, 2 or 3!")
             
-# function to build a parse tree
+# build a parse tree
 def buildParseTree(exp):
 
     # tokenize the expression
@@ -101,7 +101,7 @@ def buildParseTree(exp):
             raise ValueError
     return tree
 
-# function to evaluate the expression
+# evaluate the expression
 def evaluate(tree):
     leftTree = tree.getLeftTree()
     rightTree = tree.getRightTree()
@@ -126,23 +126,23 @@ def evaluate(tree):
     else:
         return tree.getKey()
 
-# function to sort the equations by length
+# sorting using merge sort method
 def mergeSort(l):
     for i in l:
         if len(l) > 1:
-            mid = int (len(l)/2) # Split the length of list into 2 to speed up
-            leftHalf = l[:mid] # Left side of the list
-            rightHalf = l[mid:] # Right side of list
+            mid = int (len(l)/2) 
+            leftHalf = l[:mid]
+            rightHalf = l[mid:] 
             mergeSort(leftHalf)
             mergeSort(rightHalf)
             
-            # Decalare the starting indexes as 0
+            # declare the starting indexes as 0
             leftIndex,rightIndex,mergeIndex = 0,0,0
             
-            # Declare mergeList as l (list)
+            # declare mergeList as l (list)
             mergeList = l
             
-            # Handle those items still left in both the left half and right half
+            # handle those items still left in both the left half and right half
             while leftIndex < len(leftHalf) and rightIndex < len(rightHalf):
                 if leftHalf[leftIndex] < rightHalf[rightIndex]:
                     mergeList[mergeIndex] = leftHalf[leftIndex]
@@ -152,24 +152,75 @@ def mergeSort(l):
                     rightIndex+=1
                 mergeIndex+=1
 
-            # Handle those items still left in the left Half
+            # handle those items still left in the left Half
             while leftIndex < len(leftHalf):
                 mergeList[mergeIndex] = leftHalf[leftIndex]
                 leftIndex+=1
                 mergeIndex+=1
                 
-            # Handle those items still left in the right Half
+            # handle those items still left in the right Half
             while rightIndex < len(rightHalf):
                 mergeList[mergeIndex] = rightHalf[rightIndex]
                 rightIndex+=1
                 mergeIndex+=1
 
+# change the positions of key and value in the dictionary arranged from smallest val to largest val
+def order(x, y):
+    if x[1] < y[1]: 
+        return x, y
+    else: 
+        return y, x
+        
+# do a bubble sort to sort by value
+def bubble(mydict):
+    d_items = list(mydict.items())
+    for j in range(len(d_items) - 1):
+        for i in range(len(d_items) - 1):
+            d_items[i], d_items[i+1] = order(d_items[i], d_items[i+1])
+    return d_items
+
+# sort by equation length
+def sortLength(lists):
+    # this is the index to determine location in the big list
+    index = 0 
+    for x in lists:
+        # if it is a single element list
+        if len(x) == 1:
+            index += 1
+            continue
+        else:
+            eqn_list = []
+            # if it is a multiple element lists
+            for n in x: # loop through the big list
+                eqn_list.append(Expression2(n[0]))
+
+            mergeSort(eqn_list)
+            temp_list = []
+
+            for i in range(0, len(eqn_list), 1):
+                temp_tuple = list(x[i])
+                temp_tuple[0] = eqn_list[i]
+                back_tuple = tuple(temp_tuple)
+                temp_list.append(back_tuple)
+
+            lists[index] = temp_list
+            index += 1
+    return lists
+
+# encapsulate each item in the list with [] to allow for comparison with another list
+def extractDigits(lst): 
+    res = [] 
+    for el in lst: 
+        sub = el.split(', ') 
+        res.append(sub) 
+    return(res) 
+
 # main functions
 # function to carry out choice 1  
 def choice1():
     exp = input("Please enter the expression you want to evaluate: \n")
-    print()
     tree = buildParseTree(exp)
+    print()
     print("How do you want to print the parse tree?")
     print("  1. Pre-order")
     print("  2. Post-order")
@@ -217,13 +268,15 @@ def choice2():
     print()
     inputFile = input("Please enter input file: ")
     while True:
-        if os.path.isfile(inputFile): break
+        if os.path.isfile(inputFile): 
+            break
         else:
             print('Invalid file name! File not found!')
             inputFile = input("Please enter valid input file: ")        
     outputFile = input("please enter output file: ")
     print()
-    print(">>>Evaluation and sorting started")
+    print(">>>Evaluation and sorting started:")
+    print()
     l = sortedList()
 
     # read expressions from input file and sort in list
@@ -231,89 +284,70 @@ def choice2():
     for expressions in f:
         expressions = expressions.strip()
         tree = buildParseTree(expressions)
-
         # here we evaluate the expression and then sort by value
         eqn_list.append(expressions)
         l.insert(Expression(evaluate(tree)))
         unstructured_list.append(Expression(evaluate(tree)))
     f.close()
 
-    # make a dictionary for the eqn and the values
+    # make a dictionary for the equation and values
     dictionary = dict(zip(eqn_list, unstructured_list))
 
-    # change the positions of key and value in the dictionary arranged from smallest val to largest val
-    def order(x, y):
-        if x[1] < y[1]: return x, y
-        else: return y, x
-            
-    # do a bubble sort
-    def bubble(mydict):
-        d_items = list(mydict.items())
-        for j in range(len(d_items) - 1):
-            for i in range(len(d_items) - 1):
-                d_items[i], d_items[i+1] = order(d_items[i], d_items[i+1])
-        return d_items
-        
+    # bubble sort the dictionary
     sorted_tuples = bubble(dictionary)
     new_list = []
+
+    # loop through the tuples in the list and encapsulate tuples with similar values together with []
     for n in sorted_tuples:
-        if len(new_list) == 0: new_list.append([n])
+        if len(new_list) == 0: 
+            new_list.append([n])
         else:
             if new_list[-1][-1][1] == n[1]:
                 new_list[-1].append(n)
-            else: new_list.append([n])
+            else: 
+                new_list.append([n])
     
-    def sortLength(lists):
-        index = 0 # this is the index to determine location in the big list
-        for x in lists:
-            # if it is a single element list
-            if len(x) == 1:
-                index += 1
-                continue
-            else:
-                lista = []
-                # if it is a multiple element lists
-                for n in x: # loop through the big list
-                    lista.append(Expression2(n[0]))
-
-                mergeSort(lista)
-                
-                temp_list = []
-                for i in range(0, len(lista), 1):
-                    temp_tuple = list(x[i])
-                    temp_tuple[0] = lista[i]
-                    back_tuple = tuple(temp_tuple)
-                    temp_list.append(back_tuple)
-
-                lists[index] = temp_list
-                index += 1
-        return lists
-
-    print(sortLength(new_list), "HIHI")
+    # sort the list that is sorted by value based on length of equation
+    finalSorted = sortLength(new_list)
     
+    temp_valList = [[x[1] for x in l] for l in finalSorted]
+    expList = [[x[0] for x in l] for l in finalSorted]
+    valList = []
+    valsList = []
 
+    # remove duplicate values
+    for i in temp_valList:
+        for x in i:
+            if x not in valList:
+                valList.append(x)
+    
+    # get rid of quotes from values
+    for g in valList:
+        value = str(g).strip('"\'')
+        valsList.append(value)
 
-
-    # append into the HashTable
-    # size = len(eqn_list)
-    # eqnTable = HashTable(size)
-
-
-
-    # for i in range(size):
-    #     eqnTable[i] = 
-    # HashTable.sorting(eqn_list, unstructured_list, structured_list)
-
-
+    # get the list back with each item encapsulated by []
+    valList = extractDigits(valsList)   
+    
+    # zip valList and expList so that we can compare the 2 list
+    # then we print based on list index
+    for val, exp in zip(valList, expList):
+        result = val[0]
+        print(f'*** Expressions with value = {result}')
+        for e in exp:
+            print(f'{e} ==> {result}')
+        print()
+        
     # write sorted expressions into output file
-    # f = open(outputFile, 'w')
-    # Expression = l.resetForIteration()
-    # while Expression != None:
-    #     f.write(float(str(Expression.name+"\n")))
-    #     Expression = l.nextNode()
-    # f.close()
+    f = open(outputFile, 'w')
+    for val, exp in zip(valList, expList):
+        result = val[0]
+        f.write(f'*** Expressions with value = {result}\n')
+        for e in exp:
+            f.write(f'{e} ==> {result}\n')
+        f.write('\n')
+    f.close()
 
-    
     print(">>>Evaluation and sorting completed!")
     input("Press any key, to continue....")
     selectionMenu()
